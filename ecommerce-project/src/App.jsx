92782a2fc9dd1deb51cbd0f69386d7544f145e4f
aayushhOP <aayushkmrk1441@gmail.com>
2025-12-01 14:56:50 +0530

    split header into a component and use link in place of <a>

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { Orders } from './pages/Orders'
 import './App.css'
-
+import { TrackingPage } from './pages/TrackingPage'
 
 
 function App() {
@@ -18,6 +19,16 @@ function App() {
       <Route 
         path = "checkout" 
         element = {<CheckoutPage />}
+      />
+      
+      <Route 
+        path = "orders"
+        element = {<Orders />}
+      />
+
+      <Route
+        path  = "tracking"
+        element = {<TrackingPage />} 
       />
 
     </Routes>
