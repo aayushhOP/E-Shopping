f26a463cbedcf987b02e16d60b97ae120fcd7ea2
aayushhOP <aayushkmrk1441@gmail.com>
2025-12-03 12:47:03 +0530

    seperate into smaller components

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import { Routes, Route } from 'react-router'
 import { useState, useEffect } from 'react'
-import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckoutPage'
-import { Orders } from './pages/Orders'
+import { HomePage } from './pages/home/HomePage'
+import { CheckoutPage } from './pages/checkout/CheckoutPage'
+import { Orders } from './pages/orders/Orders'
 import './App.css'
 import { TrackingPage } from './pages/TrackingPage'
 
