3978fbec337aa33f213a87813c2beb9766249657
aayushhOP <aayushkmrk1441@gmail.com>
2025-12-06 00:40:10 +0530

    converted the project into typescript

---
+++ ecommerce-project-js/README.md
@@ -0,0 +1,1 @@
+Built on React and backend is powered by Node.js, it is an ecommerce website for ordering different products listed on the website , all components are working and tested on vitest. 
---
+++ ecommerce-project/src/App.tsx
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import { Routes, Route } from 'react-router'
+import { useState, useEffect } from 'react'
+import { HomePage } from './pages/home/HomePage'
+import { CheckoutPage } from './pages/checkout/CheckoutPage'
+import { Orders } from './pages/orders/Orders'
+import './App.css'
+import { TrackingPage } from './pages/TrackingPage'
+
+
+function App() {
+  const [cart, setCart,] = useState([]);
+
+  const loadCart = async () => {
+    const response = await axios.get('/api/cart-items?expand=product')
+    setCart(response.data);
+  }
+
+  useEffect(() =>{
+   loadCart();     
+  }, []);
+  
+  
+  return (
+    <Routes>
+
+      <Route 
+        index        // or path = "/" 
+        element = {<HomePage cart={cart} loadCart = {loadCart} />}
+      />
+
+      <Route 
+        path = "checkout" 
+        element = {<CheckoutPage cart = {cart} loadCart={loadCart} />}
+      />
+      
+      <Route 
+        path = "orders"
+        element = {<Orders cart = {cart} loadCart={loadCart}/>}
+      />
+
+      <Route
+        path  = "tracking"
+        element = {<TrackingPage />} 
+      />
+
+    </Routes>
+    
+    
+  )
+}
+
+export default App
